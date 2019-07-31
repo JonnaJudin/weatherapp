09f4aaff994e01961d2961359dafae5904ee2617
Jonna Judin <jonna.judin@gmail.com>
2019-07-31 17:13:19 +0300

    eslint fixes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,10 +8,8 @@ const getWeatherFromApi = async () => {
     const response = await fetch(`${baseURL}/weather`);
     return response.json();
   } catch (error) {
-    console.error(error);
+    return {};
   }
-
-  return {};
 };
 
 class Weather extends React.Component {
@@ -19,21 +17,25 @@ class Weather extends React.Component {
     super(props);
 
     this.state = {
-      icon: "",
+      icon: '',
+      description: 'current weather',
     };
   }
 
   async componentWillMount() {
     const weather = await getWeatherFromApi();
-    this.setState({icon: weather.icon.slice(0, -1)});
+    this.setState({ icon: weather.icon.slice(0, -1),
+      description: weather.description });
+    console.log(this.state.description);
   }
 
   render() {
-    const { icon } = this.state;
+    const icon = this.state.icon;
+    const desc = this.state.description;
 
     return (
       <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} /> }
+        { icon && <img alt={`Current weather: ${desc}`} src={`/img/${icon}.svg`} /> }
       </div>
     );
   }
